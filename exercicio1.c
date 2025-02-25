6719dc969a61500e329e6f5544f858a50cdaf880
llucianoeudes <llucianoeudes@icloud.com>
2025-02-25 17:46:56 -0300

    Add files via upload

---
+++ exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	
+	
+	int idade;
+	int anoNascimento;
+	printf("Digite o ano de Nascimento\n");
+	scanf("%d",&anoNascimento);
+	idade = 2025- anoNascimento;
+	printf("ESSE ANO VOCE VAI FAZER %d ANOS",idade);
+	return 0;
+	
+}
