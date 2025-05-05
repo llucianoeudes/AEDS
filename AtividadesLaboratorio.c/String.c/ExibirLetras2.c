a722661208197e12c25e14a83ce01bb4693aba97
llucianoeudes <llucianoeudes@icloud.com>
2025-05-05 20:04:28 -0300

    comentario

--- AtividadesLaboratorio.c/String.c/ExibirLetras2.c
+++ AtividadesLaboratorio.c/String.c/ExibirLetras2.c
@@ -6,13 +6,14 @@
 int main()
 {
     char nome[]= "LUCIANO";
-    int tamanho = strlen(nome); //
-    if (tamanho >= 4) // 
+    int tamanho = strlen(nome); //função para saber o tamanho da string
+
+    if (tamanho >= 4) // verifica se o tamnho da string eh maior que 4 
     {
-        nome[tamanho - 4] = '\0'; 
+        nome[tamanho - 4] = '\0'; // eu diminuo os caracter tirando o /0
     }
 
-    printf("String modificada %s\n",nome);
+    printf("String modificada %s\n",nome); //printo 
 
     return 0;
 
