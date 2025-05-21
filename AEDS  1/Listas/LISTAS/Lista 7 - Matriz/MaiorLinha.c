0a9b3b17c99f95a9b7f5ae0a11d10ed90581f2aa
llucianoeudes <llucianoeudes@icloud.com>
2025-05-21 14:38:31 -0300

    matriz

--- AEDS  1/Listas/LISTAS/Lista 7 - Matriz/MaiorLinha.c
+++ AEDS  1/Listas/LISTAS/Lista 7 - Matriz/MaiorLinha.c
@@ -14,10 +14,17 @@ float MaiorValorMatriz(float Matriz[][100], int linha, int coluna)
     }
 
 
+
+
     int main()
     {
         int colunas;
         int linhas;
+        float matriz[linhas][colunas];
+        float numero;
+
+
+        //pede ao usuario as colunas e as linhas da matriz
 
         printf("\n Digite o numero de colunas da sua matriz:\n");
         scanf("%d",&colunas);
@@ -25,6 +32,14 @@ float MaiorValorMatriz(float Matriz[][100], int linha, int coluna)
         printf("\n Digite o numero de linhas da sua matriz:\n");
         scanf("%d",&linhas);
     
+        // preencher a matriz
 
-
+        for(int i = 0 ; i < linhas ; i++)
+            {
+                for(int j = 0 ; j < colunas ; j++)
+                    {
+                        printf("\n Digite um numero para preencher a matriz:\n");
+                        scanf("%f",&numero);
+                    }
+            }
     }
