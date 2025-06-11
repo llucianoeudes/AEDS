638aab3915634dd66f38e9e335826e25edaf7b48
llucianoeudes <llucianoeudes@icloud.com>
2025-06-11 19:51:27 -0300

    exemplo

---
+++ LISTAS/Lista 11 - C++/questao2.cpp
@@ -0,0 +1,24 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+EM C++
+
+class Retangulo 
+    {
+        int largura;
+        int altura;
+
+        int area()
+            {
+                return largura * altura;
+            }
+
+        int perimetro()
+            {
+                return 2 * (largura + altura);
+            }    
+    }
+            
+    */
